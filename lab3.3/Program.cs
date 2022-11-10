cb4573e659634f6c0ac6c1cb02843ff6019e971d
glebe <glebe@BELG04>
2022-11-11 00:27:47 +0200

    Добавьте файлы проекта.

---
+++ lab3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+//
+Console.Write("Ведите на сколько пролдолжить: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Ведите чему = x: ");
+int x = int.Parse(Console.ReadLine());
+
+
+double sum=0;
+//double c = Math.Cos(n * x) / n;
+
+for (int i=1;i<=n;i++)
+{
+    sum+=Math.Cos(i * x) / i; 
+ 
+
+}
+Console.WriteLine(sum);
+
+
+
+//double v= Math.Cos(n*x)/n
